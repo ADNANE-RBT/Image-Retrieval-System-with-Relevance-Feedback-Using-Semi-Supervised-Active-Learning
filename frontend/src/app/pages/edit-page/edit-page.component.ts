6a08ab09b58ec3f7c9a52a5a3ec63db100c2c7ba
ADNANE-RBT <92606656+ADNANE-RBT@users.noreply.github.com>
2024-12-10 18:40:42 +0100

    new pages added

---
+++ frontend/src/app/pages/edit-page/edit-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-page',
+  imports: [],
+  templateUrl: './edit-page.component.html',
+  styles: ``
+})
+export class EditPageComponent {
+
+}
