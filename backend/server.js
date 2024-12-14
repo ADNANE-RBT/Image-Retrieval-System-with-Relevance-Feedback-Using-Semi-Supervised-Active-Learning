8174623ed3e06f5534e15c714a69e422d13aea31
ADNANE-RBT <92606656+ADNANE-RBT@users.noreply.github.com>
2024-12-14 16:21:53 +0100

    added http calls

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 
 // Load environment variables
 dotenv.config();
@@ -13,6 +15,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// enable cors:
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
