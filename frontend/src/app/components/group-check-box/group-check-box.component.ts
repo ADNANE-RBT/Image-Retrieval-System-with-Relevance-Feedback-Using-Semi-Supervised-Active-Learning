5deff2fbb80f35f4648e12f52ce1ec53ee71a15e
ADNANE-RBT <92606656+ADNANE-RBT@users.noreply.github.com>
2024-12-14 16:34:03 +0100

    fixed groups checks

--- frontend/src/app/components/group-check-box/group-check-box.component.ts
+++ frontend/src/app/components/group-check-box/group-check-box.component.ts
@@ -9,12 +9,12 @@ import { Component } from '@angular/core';
 })
 export class GroupCheckBoxComponent {
   checkboxGroups = [
-    { id: 'checkbox-08-1', label: 'React' },
-    { id: 'checkbox-08-2', label: 'Next.js' },
-    { id: 'checkbox-08-3', label: 'Astro' },
-    { id: 'checkbox-08-4', label: 'Angular' },
-    { id: 'checkbox-08-5', label: 'Vue' },
-    { id: 'checkbox-08-6', label: 'Svelte' },
-    { id: 'checkbox-08-7', label: 'SolidJS' },
+    { id: 'checkbox-08-1', label: 'aGrass' },
+    { id: 'checkbox-08-2', label: 'bField' },
+    { id: 'checkbox-08-3', label: 'cIndustry' },
+    { id: 'checkbox-08-4', label: 'dRiverLake' },
+    { id: 'checkbox-08-5', label: 'eForest' },
+    { id: 'checkbox-08-6', label: 'fResident' },
+    { id: 'checkbox-08-7', label: 'gParking' },
   ];
 }
